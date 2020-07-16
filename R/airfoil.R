49b9b14840230086487a304a44a886057084dc5f
Matias Salibian-Barrera <matias@stat.ubc.ca>
2020-07-15 17:11:00 -0700

    airfoil is a data frame. added help page

---
+++ R/airfoil.R
@@ -0,0 +1,22 @@
+#' Airfoil data
+#'
+#' Here goes a description of the data.
+#'
+#' Here goes a more detailed description of the data.
+#' There are 1503 observations and 6 variables:
+#' \code{y}, \code{frequency}, \code{angle}, \code{chord_length},
+#' \code{velocity}, and \code{thickness}.
+#'
+#' @docType data
+#'
+#' @usage data(airfoil)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @references Brooks, T. F., Pope, D. S., and Marcolini, M. A. (1989). Airfoil self-noise and prediction. NASA Reference Publication-1218, document id: 9890016302.
+#'
+#' @source The UCI Archive https://archive.ics.uci.edu/ml/datasets/Airfoil+Self-Noise,
+#'
+#' @examples
+#' data(airfoil)
+"airfoil"
